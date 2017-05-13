9b1f8051cc194e743ce5ed2fffc1ccd8a38c2783
zhwang3 <zhwang_info@foxmail.com>
2017-05-14 07:35:31 +0800

    put this module to a function

--- xlio.py
+++ xlio.py
@@ -1,23 +1,23 @@
 import xlrd
-#file address
-data=xlrd.open_workbook("")
-table=data.sheet_by_index(0)
-lnum=table.nrows
-datalist=[]
-for i in range(1,lnum):
-    datalist.append(table.row_values(i))
-varname=table.row_values(0)
-dataset=[]
-data0={}
-c=0
-vn=0
-while c<len(datalist):
-    ro = datalist[c]
-    while vn<len(varname):
-        data0[varname[vn]] = ro[vn]
-        vn = vn + 1
-    dataset.append(data0)
-    c = c + 1
-    vn=0
-    data0={}
-print dataset
+def fileinput(fileaddress):
+    data = xlrd.open_workbook(fileaddress)
+    table = data.sheet_by_index(0)
+    lnum = table.nrows
+    datalist = []
+    for i in range(1, lnum):
+        datalist.append(table.row_values(i))
+    varname = table.row_values(0)
+    dataset = []
+    data0 = {}
+    c = 0
+    vn = 0
+    while c < len(datalist):
+        ro = datalist[c]
+        while vn < len(varname):
+            data0[varname[vn]] = ro[vn]
+            vn = vn + 1
+        dataset.append(data0)
+        c = c + 1
+        vn = 0
+        data0 = {}
+    return dataset
